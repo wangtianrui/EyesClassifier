467914c515be88127d5363c00488a8fb36933d2f
ScorpioMiku <1056992492@qq.com>
2018-03-22 09:31:35 +0800

    1

---
+++ SInglePicture.py
@@ -0,0 +1,38 @@
+import tensorflow as tf
+import numpy as np
+import OCnet
+
+test_batch_size = 1
+num_class = 2
+
+CHECKPOINT = "F:/Traindata/eyes/result/"  #
+
+
+def inference():
+    image = tf.ones
+    logits = OCnet.inference(image, test_batch_size, num_class)
+    result = tf.arg_max(logits, 1)
+    saver = tf.train.Saver()
+
+    with tf.Session() as sess:
+        coord = tf.train.Coordinator()  # 创建一个协调器，管理线程
+        threads = tf.train.start_queue_runners(coord=coord)  # 启动QueueRunner, 此时文件名队列已经进队。
+        ckpt = tf.train.get_checkpoint_state(CHECKPOINT)
+        i = 0
+        if ckpt and ckpt.model_checkpoint_path:
+            saver.restore(sess, ckpt.model_checkpoint_path)
+            try:
+                while not coord.should_stop() and i < 1:
+                    # image = sess.run(image_batch)
+                    # print(image)
+
+                    i += 1
+            except tf.errors.OutOfRangeError:
+                print('done!')
+            finally:
+                coord.request_stop()
+            coord.join(threads)
+            sess.close()
+
+
+if __name__ == "__main__":
